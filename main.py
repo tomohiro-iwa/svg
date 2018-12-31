67517c07f0a461882070d24d0c934bd93f14c130
tomohiro-iwa <tomo_develop_singularity_111@ezweb.ne.jp>
2018-12-31 18:51:52 +0900

    方針変更w

--- main.py
+++ main.py
@@ -2,10 +2,30 @@ import math
 
 import SVG
 
-def LSTM(x,y):
-	txt = SVG.rect(x,y,100,20)
-	return txt
+class Box:
+	def __init__(self,x,y):
+		self.x = x
+		self.y = y
+		self.w = 100
+		self.h = 50
+		return txt
 
+	def get(self,direction):
+		if direction=="u":
+			return ( self.x+(self.w/2.0), self.y )
+		if direction=="d":
+			return ( self.x+(self.w/2.0), self.y+self.h )
+		if direction=="l":
+			return ( self.x, self.y+(self.h/2.0) )
+		if direction=="r":
+			return ( self.x+, self.y+(self.h/2.0) )
+	def tostr(self):
+		return '<use href="#lstm" transform="translate(100,100)"></use>' % (self.x,self.y)
+
+boxes = []
+
+for i in range(6)
+	boxes.append(Box())
 
 
 g = SVG.SVG(800,1200)
